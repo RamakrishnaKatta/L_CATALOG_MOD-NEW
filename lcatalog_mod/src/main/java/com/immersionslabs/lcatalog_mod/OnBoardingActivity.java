034a7aa9b99ad035ca1504c227a194ca1720fac0
Raveendra Babu Aripaka <raveendra.a@immersionslabs.com>
2018-01-17 17:23:13 +0530

    SplashScreen // OnBoarding // MainActivity

---
+++ lcatalog_mod/src/main/java/com/immersionslabs/lcatalog_mod/OnBoardingActivity.java
@@ -0,0 +1,305 @@
+package com.immersionslabs.lcatalog_mod;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.immersionslabs.lcatalog_mod.Utils.NetworkConnectivity;
+import com.immersionslabs.lcatalog_mod.Utils.PrefManager;
+
+public class OnBoardingActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private LinearLayout dotsLayout;
+    private int[] layouts;
+    private Button btnSkip, btnNext;
+    private PrefManager prefManager;
+
+
+    TextView welcome_11, welcome_13, welcome_21, welcome_31, welcome_32, welcome_41;
+    TextView welcome_14, welcome_22, welcome_42, welcome_43;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Checking for first time launch - before calling setContentView()
+        prefManager = new PrefManager(this);
+        if (!prefManager.WelcomeActivityScreenLaunch()) {
+            launchHomeScreen();
+            finish();
+        }
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+
+        // Making notification bar transparent
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+        setContentView(R.layout.activity_onboarding);
+
+        viewPager = findViewById(R.id.view_pager);
+        dotsLayout = findViewById(R.id.layoutDots);
+        btnSkip = findViewById(R.id.btn_skip);
+        btnSkip.setTextSize(20);
+        btnSkip.setTypeface(btnSkip.getTypeface(), Typeface.BOLD_ITALIC);
+        btnNext = findViewById(R.id.btn_next);
+        btnNext.setTextSize(20);
+        btnNext.setTypeface(btnNext.getTypeface(), Typeface.BOLD_ITALIC);
+
+        // layouts of all welcome sliders
+        // add few more layouts if you want
+        layouts = new int[]{
+                R.layout.onboarding_slide1,
+                R.layout.onboarding_slide2,
+                R.layout.onboarding_slide3,
+                R.layout.onboarding_slide4};
+
+        // adding bottom dots
+        addBottomDots(0);
+
+        // making notification bar transparent
+        changeStatusBarColor();
+
+        MyViewPagerAdapter myViewPagerAdapter = new MyViewPagerAdapter();
+        viewPager.setAdapter(myViewPagerAdapter);
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+
+        btnSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchHomeScreen();
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // checking for last page
+                // if last page home screen will be launched
+                int current = getItem(+1);
+                if (current < layouts.length) {
+                    // move to next screen
+                    viewPager.setCurrentItem(current);
+                } else {
+                    launchHomeScreen();
+                }
+            }
+        });
+
+        if (NetworkConnectivity.checkInternetConnection(this)) {
+
+        } else {
+            InternetMessage();
+        }
+    }
+
+    private void InternetMessage() {
+        final View view = this.getWindow().getDecorView().findViewById(android.R.id.content);
+        final Snackbar snackbar = Snackbar.make(view, "Check Your Internet connection", Snackbar.LENGTH_INDEFINITE);
+        snackbar.setAction("RETRY", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                snackbar.dismiss();
+                if (NetworkConnectivity.checkInternetConnection(OnBoardingActivity.this)) {
+
+                } else {
+
+                    InternetMessage();
+                    // CustomMessage.getInstance().CustomMessage(this,"Check Your Internet connection.");
+                }
+            }
+        });
+        snackbar.show();
+    }
+
+    private void addBottomDots(int currentPage) {
+        TextView[] dots = new TextView[layouts.length];
+
+        dotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(Color.WHITE);
+            dotsLayout.addView(dots[i]);
+        }
+
+        if (dots.length > 0)
+            dots[currentPage].setTextColor(Color.parseColor("#004D40"));
+    }
+
+    private int getItem(int i) {
+        return viewPager.getCurrentItem() + i;
+    }
+
+    private void launchHomeScreen() {
+        prefManager.SetWelcomeActivityScreenLaunch(false);
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+    }
+
+    //	viewpager change listener
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageSelected(int position) {
+            addBottomDots(position);
+
+            // changing the next button text 'NEXT' / 'GOT IT'
+            if (position == layouts.length - 1) {
+                // last page. make button text to GOT IT
+                btnNext.setText(getString(R.string.start));
+                btnSkip.setVisibility(View.GONE);
+            } else {
+                // still pages are left
+                btnNext.setText(getString(R.string.next));
+                btnSkip.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+        }
+    };
+
+    /**
+     * Making notification bar transparent
+     */
+    private void changeStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
+    /**
+     * View pager adapter
+     */
+    public class MyViewPagerAdapter extends PagerAdapter {
+        private LayoutInflater layoutInflater;
+
+        MyViewPagerAdapter() {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            assert layoutInflater != null;
+            View view = layoutInflater.inflate(layouts[position], container, false);
+
+            Typeface custom_font = Typeface.createFromAsset(getAssets(), "fonts/Graduate-Regular.ttf");
+            Typeface custom_font2 = Typeface.createFromAsset(getAssets(), "fonts/Cookie-Regular.ttf");
+
+            // Titles
+            welcome_11 = view.findViewById(R.id.txt_welcome_11);
+            if (welcome_11 != null) {
+                welcome_11.setTypeface(custom_font);
+            }
+
+            welcome_13 = view.findViewById(R.id.txt_welcome_13);
+            if (welcome_13 != null) {
+                welcome_13.setTypeface(custom_font);
+            }
+
+            welcome_31 = view.findViewById(R.id.txt_welcome_31);
+            if (welcome_31 != null) {
+                welcome_31.setTypeface(custom_font);
+            }
+
+            welcome_41 = view.findViewById(R.id.txt_welcome_41);
+            if (welcome_41 != null) {
+                welcome_41.setTypeface(custom_font);
+            }
+
+            welcome_42 = view.findViewById(R.id.txt_welcome_42);
+            if (welcome_42 != null) {
+                welcome_42.setTypeface(custom_font);
+            }
+
+            welcome_21 = view.findViewById(R.id.txt_welcome_21);
+            if (welcome_21 != null) {
+                welcome_21.setTypeface(custom_font);
+            }
+
+            //SubTitles
+            welcome_22 = view.findViewById(R.id.txt_welcome_22);
+            if (welcome_22 != null) {
+                welcome_22.setTypeface(custom_font2);
+            }
+
+            welcome_43 = view.findViewById(R.id.txt_welcome_43);
+            if (welcome_43 != null) {
+                welcome_43.setTypeface(custom_font2);
+            }
+
+            welcome_32 = view.findViewById(R.id.txt_welcome_32);
+            if (welcome_32 != null) {
+                welcome_32.setTypeface(custom_font2);
+            }
+
+            welcome_14 = view.findViewById(R.id.txt_welcome_14);
+            if (welcome_14 != null) {
+                welcome_14.setTypeface(custom_font2);
+            }
+
+            container.addView(view);
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return layouts.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public void onBackPressed() {
+        setResult(RESULT_CANCELED);
+        super.onBackPressed();
+        finish();
+    }
+}
